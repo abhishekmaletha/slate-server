e5841b8d7576a07c34b21baffebc4383e065ade3
abhishekmaletha <abhishek.maletha2000@gmail.com>
2021-07-21 01:18:54 +0530

    server change stand aside

---
+++ server.js
@@ -0,0 +1,51 @@
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http, {
+    cors: {
+        origin: "*",
+    },
+});
+// let connections = [];
+const {
+    userJoin,
+    showUsers,
+    getCurrentUser,
+    userLeave,
+    getRoomUsers
+} = require("./utils");
+
+io.on("connect", (socket) => {
+    // connections.push(socket);
+    socket.on('joinRoom', (data) => {
+        console.log(data);
+        const user = userJoin(socket.id, data.user, data.room);
+        socket.join(user.room);
+        console.log(`${user.username} has joined ${user.room}`);
+
+        // Send users and room info
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+        // showUsers();
+    });
+    // console.log(`${socket.id} has connected`);
+    socket.on('send', (data) => {
+        showUsers();
+        console.log(socket.id);
+        console.log('data in sever ', data);
+        const user = getCurrentUser(socket.id);
+        console.log('curr user', user);
+        io.to(user.room).emit('draw', data);
+    });
+    //Disconnect
+    socket.on("disconnect", () => {
+        console.log(`${socket.id} was disconnected`);
+        // connections = connections.filter((con) => con.id !== socket.id);
+    });
+});
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 4000;
+http.listen(server_port, () => {
+    console.log("Started on : " + server_port);
+});
