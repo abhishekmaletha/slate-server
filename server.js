268e3860b132757f122a54eef41702e0c5c73e7f
abhishek maletha <git config --get-all user.name>
2021-10-28 11:29:47 +0530

    cors fix 1.2

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 var app = require("express")();
 var http = require("http").createServer(app);
-var io = require("socket.io")(http, {
-    cors: {
-        origin: "*",
-    },
-});
+var io = require("socket.io")(http);
 // let connections = [];
 let cors = require("cors");
 app.use(cors());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT ,DELETE");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+});
 const {
     userJoin,
     showUsers,
